a1948116e4e6fe5a7a1d9485e9ca646f3ec0ea19
ksn <scofield.michael339@gmail.com>
2025-09-26 22:23:31 +0700

    Feat: Added ec2 public dns output in output.tf

--- Infra/output.tf
+++ Infra/output.tf
@@ -42,3 +42,8 @@ output "ec2_public_ip" {
   description = "Public IP of the EC2 instance"
   value       = module.ec2-instance.public_ip
 }
+
+output "ec2_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = module.ec2-instance.public_dns
+}
